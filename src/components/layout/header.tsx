6e46c1f81126e8854428df8779e2fc8bbd0f2727
Tanmay Patil <tanmaygpatil@gmail.com>
2025-07-26 06:07:12 +0000

    u forgot to apply css and all the chnages

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -20,10 +20,11 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { SidebarTrigger } from "../ui/sidebar";
+import { ThemeToggle } from "../theme-toggle";
 
 export function AppHeader() {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-transparent px-4 sm:static sm:h-auto sm:border-0 sm:px-6">
       <SidebarTrigger className="sm:hidden" />
       <div className="flex items-center gap-2 text-sm text-muted-foreground">
         <Link href="#" className="hover:text-foreground">
@@ -40,6 +41,7 @@ export function AppHeader() {
           className="w-full rounded-lg bg-secondary pl-8 md:w-[200px] lg:w-[320px]"
         />
       </div>
+      <ThemeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
