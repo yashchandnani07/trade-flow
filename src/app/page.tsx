bf967420afc56f69634f38d26e40ff82b63496e0
Tanmay Patil <tanmaygpatil@gmail.com>
2025-07-26 06:10:57 +0000

    **Prompt for AI to Generate a Sidebar Functionality with Glass Effect an

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,30 +9,32 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <AppHeader />
-        <main className="flex-1 p-4 md:p-6 lg:p-8 space-y-8 bg-background">
-          <OverviewCards />
-          <div className="grid gap-8 lg:grid-cols-3">
-            <div className="lg:col-span-2">
-              <OrderTracking />
+    <div className="bg-gradient-to-br from-indigo-900 via-blue-900 to-purple-900 min-h-screen">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <AppHeader />
+          <main className="flex-1 p-4 md:p-6 lg:p-8 space-y-8 bg-transparent">
+            <OverviewCards />
+            <div className="grid gap-8 lg:grid-cols-3">
+              <div className="lg:col-span-2">
+                <OrderTracking />
+              </div>
+              <div>
+                <AlertsSection />
+              </div>
             </div>
-            <div>
-              <AlertsSection />
+            <div className="grid gap-8 lg:grid-cols-5">
+              <div className="lg:col-span-3">
+                <OrderHistory />
+              </div>
+              <div className="lg:col-span-2">
+                <SupplierReviews />
+              </div>
             </div>
-          </div>
-          <div className="grid gap-8 lg:grid-cols-5">
-            <div className="lg:col-span-3">
-              <OrderHistory />
-            </div>
-            <div className="lg:col-span-2">
-              <SupplierReviews />
-            </div>
-          </div>
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
   );
 }
