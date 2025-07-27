433b723a52c2824b60604c6689b9c5c5506569f5
Siddhesh Hirve <ram.sidrhirve@gmail.com>
2025-07-27 12:51:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/bids/marketplace-bids-list.tsx
+++ src/components/bids/marketplace-bids-list.tsx
@@ -345,7 +345,7 @@ export function MarketplaceBidsList() {
 
             {loading && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {[...Array(3)].map((_, i) => <Skeleton key={`skeleton-${i}`} className="h-64" />)}
+                    {[...Array(3)].map((_, i) => <Skeleton key={`bid-skeleton-${i}`} className="h-64" />)}
                 </div>
             )}
 
@@ -358,12 +358,14 @@ export function MarketplaceBidsList() {
             )}
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {!loading && bids && bids.map(bid => (
-                    <BidCard key={bid.id} bid={bid} />
-                ))}
+                {!loading &&
+                    bids &&
+                    bids
+                        .filter(bid => !!bid.id) // Ensure bid has an ID
+                        .map(bid => (
+                            <BidCard key={bid.id} bid={bid} />
+                        ))}
             </div>
         </div>
     );
 }
-
-    
